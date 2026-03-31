933bfb32c9c882b732654b5cd4cc58d7c5cd9717
Sandra Blanco <sandra.blanco18@gmail.com>
2026-03-31 11:35:28 +0200

    changes on data/levels.js

--- data/levels.js
+++ data/levels.js
@@ -1,10 +1,11 @@
 const levels = [
   {
     level: 1,
+    story:'Hace no mucho que acabas de salir de una relación larga de años. Estas en esa etapa de no quiero saber nada de nadie y has experimentado mucho alivio al dejar tu relación porque ya no erais un equipo y tu sistema nervioso estaba experimentado que ahí no era. Pero por arte de magia vienen alguien muy majo que empieza a hablarte y te hace reir, descubres que tenéis muchas cosas en común y decides acudir a la cita. Notas que conectáis un montón, total que tu hace no mucho que hiciste un bautismo de buceo y resulta que el chico en cuestión también bucea y le encanta y te plantea lo siguiente: '
     type: 'riddle',
     question: '¿Por qué los buzos siempre se tiran de espaldas?',
     options: [
-        'Porque si se tira mi amigo lo hago yo tambien',
+        'Porque si se tira mi amigo/a lo hago yo también',
         'Porque si lo dice el capitán va a misa',
         'Porque si se tiran de frente caen dentro de la lancha'
     ],
