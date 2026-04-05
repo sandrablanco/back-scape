26d45adb6dda7d5ac949993903dbb31e9fcb7453
Sandra Blanco <sandra.blanco18@gmail.com>
2026-04-05 19:28:03 +0200

    modified data/levels.js

--- data/levels.js
+++ data/levels.js
@@ -1,7 +1,7 @@
 const levels = [
   {
     level: 1,
-    story:'Hace no mucho que acabas de salir de una relación larga de años. Estas en esa etapa de no quiero saber nada de nadie y has experimentado mucho alivio al dejar tu relación porque ya no erais un equipo y tu sistema nervioso estaba experimentado que ahí no era. Pero por arte de magia vienen alguien muy majo que empieza a hablarte y te hace reir, descubres que tenéis muchas cosas en común y decides acudir a la cita. Notas que conectáis un montón, total que tu hace no mucho que hiciste un bautismo de buceo y resulta que el chico en cuestión también bucea y le encanta y te plantea lo siguiente: ',
+    story:'Hace no mucho que acabas de salir de una relación larga de años. Estás en esa etapa de no quiero saber nada de nadie y has experimentado mucho alivio al dejar tu relación, porque ya no érais un equipo y tu sistema nervioso estaba experimentado que ahí no era. Pero por arte de magia vienen alguien muy agradable que empieza a hablarte y te hace reir, descubres que tenéis muchas cosas en común, te invita a la cita y decides acudir. Notas que conectáis un montón, total que tu hace no mucho que hiciste un curso de iniciación de buceo y resulta que el chico en cuestión también bucea y le encanta y te plantea lo siguiente: ',
     type: 'riddle',
     question: '¿Por qué los buzos siempre se tiran de espaldas?',
     options: [
@@ -14,7 +14,7 @@ const levels = [
   {
     level: 2,
     type: 'image',
-    story: '.Despues de la cita como has caido en sus redes, el chico finalmente consigue lo que quería y te acaba haciendo ghosting y te llevas otro chasco oh 💔. Pasa el tiempo y reaparece un amigo tuyo de hace mucho tiempo atrás de cuando ibais juntos a boxeo. Te protege como buen amigo, te propone planes, te habla casi a todas horas y te dice de acudir al museo para distraerte un rato. Llevais un rato viendo cuadros pero uno en especial llama vuestra atencion. El chico te pregunta: ',
+    story: '.Despues de la cita, como has caido en sus redes, el chico finalmente consigue lo que quería y te acaba haciendo ghosting y te llevas otro chasco, oh 💔. Pasa el tiempo y reaparece un amigo tuyo de hace mucho tiempo atrás, de cuando ibais juntos a boxeo. Te protege como buen amigo, te propone planes, te habla casi a todas horas y te propone acudir al museo para distraerte un rato. Llevais un rato viendo cuadros pero uno en especial llama vuestra atención. El chico te pregunta: ',
     image: 'https://wl-genial.cf.tsp.li/resize/1200x630/webp/cb4/cac/1bbe835e09af7466eaa9222fca.jpg.webp',
     question: '¿Qué ves?',
     correctAnswer: 'un perro'
@@ -22,16 +22,16 @@ const levels = [
   {
     level: 3,
     type: 'audio',
-    story: '.Tu veias claramente un perro y el un gato por lo que buscaste en una pagina de psicología que quien veía un gato era un viva la pepa y poco empático y quién veía un perro era leal asi que lo ves como una red flag🚩 y le mandas a donde Cristo perdió el mechero. Pasado un tiempo tienes un percance en el trabajo y decides acudir a un profesional que te asesore. Como le caes en gracia te pide que os tomeis una cerveza en el bar de aqui al lado que ponen buena música. Spoiler: te gusta el rock demasiado asi que aceptas. El chico te ha invitado a unas cuantas cervezas y avisa que viene su canción favorita, una balada rockera. Te dice: ',
-    question: 'Mira no se mucho inglés pero hay un trocito que me hace mucha gracia porque parece que dice algo en español',
+    story: '.Tu veías claramente un perro y el un gato, por lo que buscaste en una pagina de psicología que quién veía un gato era un viva la pepa y poco empático y quién veía un perro era leal, asi que lo ves como una red flag🚩 y le mandas a freir espárragos. Pasado un tiempo, tienes un percance en el trabajo y decides acudir a un profesional que te asesore. Como le caes en gracia te pide que os tomeis una cerveza en el bar de aqui al lado, donde ponen buena música. Spoiler: te gusta el rock demasiado, asi que aceptas. El chico te ha invitado a unas cuantas cervezas y avisa que viene su canción favorita, una balada rockera. Te dice: ',
+    question: 'Mira, no se mucho inglés pero hay un trocito que me hace mucha gracia porque parece que dice algo en español',
     correctAnswer: 'un chinito pecando',
     audio: 'https://res.cloudinary.com/du5e2crvt/video/upload/v1775234032/Candlelit_Glass_yjs5dw.mp3'
   },
    {
     level: 4,
     type: 'puzzle',
-    story: '.Aciertas con lo que dice la canción, que el chico se pone tan contento que se avalanza sobre ti y te besa. Tu le apartas en plan: ¿qué haces?🤨 y el otro se queda bloqueado diciendo tienes razón es que estoy en crisis con mi novia. Inmediatamente sales huyendo despavorida. Regresas a casa decepcionada y renagando de los hombres. Pasado un tiempo te llega un mensaje de tu ex que te dice que se ha dado cuenta de que no era el momento para dejarte y que quiere volver contigo. Tu le dices que no quieres saber nada de él pero el insiste y te dice que si quieres quedar para hablar e invitarte a un café. Tu aceptas porque quieres cerrar ese capítulo de tu vida y el día de la cita te propone un juego para romper el hielo como sabe que te gustan los juegos, saca una vieja caja y al destaparla y volcar las piezas sobre la mesa te dice: ',
-    question: 'No he dejado de pensar el jardin que creamos juntos. Lleno de ortensias por todos lados:',
+    story: '.Aciertas con lo que dice la canción, y el chico se pone tan contento que se avalanza sobre ti y te besa. Tu le apartas en plan: ¿qué haces?🤨 y el otro se queda bloqueado diciendo tienes razón es que estoy en crisis con mi novia. Inmediatamente sales huyendo despavorida. Regresas a casa decepcionada y renagando de los hombres. Pasado un tiempo, te llega un mensaje de tu ex que te dice que se ha dado cuenta de que no era el momento para dejarte y que quiere volver contigo. Tu le dices que no quieres saber nada de él pero el insiste y te dice que si quieres quedar para hablar e invitarte a un café. Tu aceptas porque quieres cerrar ese capítulo de tu vida y el día de la cita te propone un juego para romper el hielo como sabe que te gustan los juegos, saca una vieja caja y al destaparla y volcar las piezas sobre la mesa te dice: ',
+    question: 'No he dejado de pensar en el jardín que creamos juntos. Lleno de ortensias por todos lados:',
     pieces: [
       'https://res.cloudinary.com/du5e2crvt/image/upload/v1775238099/pieza1_cnd2j0.jpg',
       'https://res.cloudinary.com/du5e2crvt/image/upload/v1775238107/pieza2_u2sgz5.jpg',
@@ -50,15 +50,15 @@ const levels = [
   {
     level: 5,
     type: 'wordsearch',
-    story: '. Superas el reto del puzzle y te has aclarado las ideas de que con el ex no se vuelve, que por algo es ex. No obstante, aparece el ex de tu amiga un día con la excusa de verte para decirte lo mal que se encuentra. Accedes por qué como eres una buena samaritana quieres darle tu apoyo y escuchar su versión. Rápidamente los dias siguientes te empieza a dar 🔥 a tus stories en Instagram, te comenta que que guapa estás. Te manda mensajes privados y te propone un juego para conoceros mejor en el que el inevitablemente empieza a subir el tono de la conversación. Evidentemente intuyes por donde va y le paras los pies. Para evadirte un rato coges la sopa de letras de tu madre y te sorprendes al ver que hay palabras relacionadas con el tipo de relaciones tóxicas que has tenido últimamente. Te preguntas si es una señal del universo o simplemente es tu mente buscando respuestas a lo que te ha pasado. De repente te das cuenta de que hay tres palabras que se repiten una y otra vez y que parecen ser la clave para entender lo que te ha pasado.',
+    story: '. Superas el reto del puzzle y te has aclarado las ideas, llegando a la conclusión de que con el ex no se vuelve, que por algo es ex. No obstante, un día aparece el ex de tu amiga, con la excusa de verte para decirte lo mal que se encuentra. Accedes a hablar con él porque como eres una buena samaritana quieres darle tu apoyo y escuchar su versión. Rápidamente los dias siguientes te empieza a dar 🔥 a tus stories en Instagram, te comenta lo guapa que estás, lo bonito que son tus ojos,etc. Te manda mensajes privados y te propone un juego para conoceros mejor en el que el inevitablemente empieza a subir el tono de la conversación. Evidentemente intuyes por donde va y le paras los pies. Para evadirte un rato, coges los pasatiempos de tu madre y decides hacer una sopa de letras y te sorprendes al ver que hay palabras relacionadas con el tipo de relaciones tóxicas que has tenido últimamente. Te preguntas si es una señal del universo o simplemente es tu mente buscando respuestas a lo que te ha pasado. De repente te das cuenta de que hay unas palabras que se repiten una y otra vez y que parecen ser la clave para entender lo que te ha pasado.',
     question: '¿Qué palabras son?',
-    correctAnswer: [ 'LOVEBOMBING', 'BREADCRUMBING', 'GHOSTING' ],
+    correctAnswer: [ 'LOVEBOMBING', 'BREADCRUMBING', 'GHOSTING', 'GASLIGHTING', 'ZOMBIEING' ],
     
   },
    {
     level: 6,
     type: 'maze',
-    story: '. Con todo lo que te ha pasado últimamente sientes que si hicieras un guión y se lo vendieras a Netlfix sería un éxito total. Te sientes como la protagonista de una película de amor y desamor y quieres que tu historia tenga un final feliz. Esa noche tienes una pesadilla, al principio experimentas ansiedad y miedo 🥴 no sabes donde estas, pero poco a poco vas reconociendo el lugar y te das cuenta de que estas en un laberinto. El laberinto es tu mente y tienes que encontrar la salida para despertar de la pesadilla.',
+    story: '. Con todo lo que te ha pasado últimamente, sientes que si hicieras un guión y se lo vendieras a Netlfix sería un éxito total. Te sientes como la protagonista de una película de amor y desamor y quieres que tu historia tenga un final feliz. Esa noche tienes una pesadilla, al principio experimentas ansiedad y miedo 🥴 no sabes donde estás, pero poco a poco vas reconociendo el lugar y te das cuenta de que estas en un laberinto. El laberinto es tu mente y tienes que encontrar la salida para despertar de la pesadilla.',
       maze: [
       ['S', '.', 'X', '.', '.'], // S = Start, E = End, X = Wall, . = Path
       ['X', '.', 'X', '.', 'X'],
