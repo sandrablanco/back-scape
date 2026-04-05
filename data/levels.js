1082b1da9a7388546e2f0a07fcc8d3d56dcec8cc
Sandra Blanco <sandra.blanco18@gmail.com>
2026-04-05 17:36:48 +0200

    modified data/Levels.js

--- data/levels.js
+++ data/levels.js
@@ -17,14 +17,14 @@ const levels = [
     story: '.Despues de la cita como has caido en sus redes, el chico finalmente consigue lo que quería y te acaba haciendo ghosting y te llevas otro chasco oh 💔. Pasa el tiempo y reaparece un amigo tuyo de hace mucho tiempo atrás de cuando ibais juntos a boxeo. Te protege como buen amigo, te propone planes, te habla casi a todas horas y te dice de acudir al museo para distraerte un rato. Llevais un rato viendo cuadros pero uno en especial llama vuestra atencion. El chico te pregunta: ',
     image: 'https://wl-genial.cf.tsp.li/resize/1200x630/webp/cb4/cac/1bbe835e09af7466eaa9222fca.jpg.webp',
     question: '¿Qué ves?',
-    correctAnswer: 'un perro' || 'perro'
+    correctAnswer: 'un perro' && 'perro'
   },
   {
     level: 3,
     type: 'audio',
     story: '.Tu veias claramente un perro y el un gato por lo que buscaste en una pagina de psicología que quien veía un gato era un viva la pepa y poco empático y quién veía un perro era leal asi que lo ves como una red flag🚩 y le mandas a donde Cristo perdió el mechero. Pasado un tiempo tienes un percance en el trabajo y decides acudir a un profesional que te asesore. Como le caes en gracia te pide que os tomeis una cerveza en el bar de aqui al lado que ponen buena música. Spoiler: te gusta el rock demasiado asi que aceptas. El chico te ha invitado a unas cuantas cervezas y avisa que viene su canción favorita, una balada rockera. Te dice: ',
     question: 'Mira no se mucho inglés pero hay un trocito que me hace mucha gracia porque parece que dice algo en español',
-    correctAnswer: 'un chinito pecando' || 'chinito pecando' ,
+    correctAnswer: 'un chinito pecando' && 'chinito pecando' ,
     audio: 'https://res.cloudinary.com/du5e2crvt/video/upload/v1775234032/Candlelit_Glass_yjs5dw.mp3'
   },
    {
@@ -47,10 +47,18 @@ const levels = [
        'https://res.cloudinary.com/du5e2crvt/image/upload/v1775238120/pieza3_tim2jf.jpg',
        'https://res.cloudinary.com/du5e2crvt/image/upload/v1775238136/pieza6_lqstsi.jpg']
 },
-   {
+  {
     level: 5,
+    type: 'wordsearch',
+    story: '. Superas el reto del puzzle y te has aclarado las ideas de que con el ex no se vuelve, que por algo es ex. No obstante, aparece el ex de tu amiga un día con la excusa de verte para decirte lo mal que se encuentra. Accedes por qué como eres una buena samaritana quieres darle tu apoyo y escuchar su versión. Rápidamente los dias siguientes te empieza a dar 🔥 a tus stories en Instagram, te comenta que que guapa. Te manda mensajes privados y te propone un juego para conoceros mejor en el que el inevitablemente empieza a subir el tono de la conversación. Evidentemente intuyes por donde va y le paras los pies. Para evadirte un rato coges la sopa de letras de tu madre y te sorprendes al ver que hay palabras relacionadas con el tipo de relaciones tóxicas que has tenido últimamente. Te preguntas si es una señal del universo o simplemente es tu mente buscando respuestas a lo que te ha pasado. De repente te das cuenta de que hay tres palabras que se repiten una y otra vez y que parecen ser la clave para entender lo que te ha pasado.',
+    question: 'Mira no se mucho inglés pero hay un trocito que me hace mucha gracia porque parece que dice algo en español',
+    correctAnswer: 'lovebombing' && 'breadcrumbing' && 'ghosting' 
+    
+  },
+   {
+    level: 6,
     type: 'maze',
-    story: '.Has resuelto el puzzle y te has aclarado las ideas que con el ex no se vuelve porque por algo es ex. Con todo lo que te ha pasado últimamente sientes que si hicieras un guión y se lo vendieras a Netlfix sería un éxito total. Te sientes como la protagonista de una película de amor y desamor y quieres que tu historia tenga un final feliz. Esa noche tienes una pesadilla, al principio experimentas ansiedad y miedo 🥴 no sabes donde estas, pero poco a poco vas reconociendo el lugar y te das cuenta de que estas en un laberinto. El laberinto es tu mente y tienes que encontrar la salida para despertar de la pesadilla.',
+    story: '. Con todo lo que te ha pasado últimamente sientes que si hicieras un guión y se lo vendieras a Netlfix sería un éxito total. Te sientes como la protagonista de una película de amor y desamor y quieres que tu historia tenga un final feliz. Esa noche tienes una pesadilla, al principio experimentas ansiedad y miedo 🥴 no sabes donde estas, pero poco a poco vas reconociendo el lugar y te das cuenta de que estas en un laberinto. El laberinto es tu mente y tienes que encontrar la salida para despertar de la pesadilla.',
       maze: [
       ['S', '.', 'X', '.'], // S = Start, E = End, X = Wall, . = Path
       ['X', '.', 'X', '.'],
@@ -66,7 +74,7 @@ const levels = [
     
   },
      {
-    level: 6,
+    level: 7,
     type: 'the end',
     story:'. Al final consigues salir de la pesadilla y te hace click el algo y decides que estas muy bien sola, que no necesitas a nadie para ser feliz y que tu historia de amor va a ser contigo misma. Te quieres, te cuidas, te respetas y te valoras. Eres tu mejor compañía y tu mejor amiga. Y así es como termina tu historia de amor, con un final feliz contigo misma.😍',
     
